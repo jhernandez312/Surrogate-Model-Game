28c16ca903a59e6dbf5441d1d4def62676066c4e
jessica <ja.hdz309@gmail.com>
2024-10-21 00:06:58 -0400

    predict url updated

--- app/api/predict/route.ts
+++ app/api/predict/route.ts
@@ -16,7 +16,7 @@ export async function POST(request: Request) {
         } = data;
 
         // Send the form data to the Flask API for prediction
-        const response = await fetch('http://localhost:5000/predict', {
+        const response = await fetch('https://surrogate-model-game-1-vagb.onrender.com/predict', {
             method: 'POST',
             headers: {
                 'Content-Type': 'application/json',
